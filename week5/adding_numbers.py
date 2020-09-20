ba88a47c7d77d822cfeb503761423e237ee07bd0
Haize Heyrana <haizeheyrana@Haizes-MacBook-Pro.local>
2020-09-19 21:27:05 -0400

    adding homework and deleted line of code

--- week5/adding_numbers.py
+++ week5/adding_numbers.py
@@ -3,7 +3,10 @@ import sys
 def add_them_all(filename):
    sum = 0
    ### Your code here
-
+   with open(filename, "r") as file_sum:   
+       for line in file_sum:
+           sum += (int(line))
+           
    ### End of your code
    return sum
 
