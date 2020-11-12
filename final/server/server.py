88853b5c0950eb8321913c01b29cee4aa37ff855
Haize Heyrana <haizeheyrana@Haizes-MacBook-Pro.local>
2020-11-11 21:30:55 -0500

    instructions added

--- final/server/server.py
+++ final/server/server.py
@@ -1,3 +1,7 @@
+#Must accept a path /hello with a parameter for 'name'
+#Return value must be a dictionary with the key 'hello' and the value of the name url parameter
+#Server should listen on port 5000
+
 from flask import Flask
 from markupsafe import escape
 
