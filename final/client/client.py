04b7d6a80c750a9e2706c4998612405fd92ea77c
Haize Heyrana <haizeheyrana@Haizes-MacBook-Pro.local>
2020-11-11 21:31:03 -0500

    instructions added for client.py

--- final/client/client.py
+++ final/client/client.py
@@ -1,3 +1,6 @@
+#Must accept user input representing a 'name'
+#Must make HTTP GET call to server in the form of: http://localhost:5000/hello/{name}
+
 import requests
 
 r = requests.get("http://localhost:5000/hello{name}")
