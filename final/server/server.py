3c1e0c2ae833377e0812e6f585200e1427648c68
Haize Heyrana <haizeheyrana@Haizes-MacBook-Pro.local>
2020-11-12 21:20:11 -0500

    changes & learned how to use anaconda envir

--- final/server/server.py
+++ final/server/server.py
@@ -7,29 +7,30 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Index Page\n'
+@app.route('/hello/<name>')
+def hello_name(name):
+    return {"hello": name}
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World\n'
+#@app.route('/')
+#def index():
+    #return 'Index Page\n'
 
-@app.route('/dictionary')
-def dictionary():
-    return {"key": "value"}
 
-@app.route('/user/<username>')
-def show_user_profile(username):
+#@app.route('/dictionary')
+#def dictionary():
+    #return {"key": "value"}
+
+#@app.route('/user/<username>')
+#def show_user_profile(username):
     # show the user profile for that user
-    return 'User %s' % escape(username)
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
+    #return 'User %s' % escape(username)
+#@app.route('/post/<int:post_id>')
+#def show_post(post_id):
     # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
+    #return 'Post %d' % post_id
+#@app.route('/path/<path:subpath>')
+#def show_subpath(subpath):
     # show the subpath after /path/
-    return 'Subpath %s' % escape(subpath)
+    #return 'Subpath %s' % escape(subpath)
 
 
