2b40f7edc9476a4ee58f7b5d21f46e65187484ab
Haize Heyrana <haizeheyrana@Haizes-MacBook-Pro.local>
2020-11-17 21:25:20 -0500

    confused af

--- final/server/server.py
+++ final/server/server.py
@@ -22,29 +22,4 @@ def search_title(descr, c=None):
         results.append(movie)
     return results
 
-
-@app.route('/movies/bytitle/<str:title>')
-
-#@app.route('/')
-#def index():
-    #return 'Index Page\n'
-
-
-#@app.route('/dictionary')
-#def dictionary():
-    #return {"key": "value"}
-
-#@app.route('/user/<username>')
-#def show_user_profile(username):
-    # show the user profile for that user
-    #return 'User %s' % escape(username)
-#@app.route('/post/<int:post_id>')
-#def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    #return 'Post %d' % post_id
-#@app.route('/path/<path:subpath>')
-#def show_subpath(subpath):
-    # show the subpath after /path/
-    #return 'Subpath %s' % escape(subpath)
-
-
+    
